bcd59df556426f9eb287dd1fd1e5d3e534da7d83
talirrito <91436710+talirrito@users.noreply.github.com>
2021-10-01 18:00:06 -0700

    Adding the code for "while".

---
+++ astr-119-hw-2/while.py
@@ -0,0 +1,7 @@
+#super duper tiny amount of code but that's ok
+
+i=0 #defines an integer i 
+
+while(i<119):  #while i is less than 119, o the following 
+	print(i)  #prints the current value of i 
+	i+=10     #add 10 to i
