e052109f9654a63a5235dc1b79dc8dfe08f309fa
talirrito <91436710+talirrito@users.noreply.github.com>
2021-10-01 18:01:41 -0700

    adding code for "operators".

---
+++ astr-119-hw-2/operators.py
@@ -0,0 +1,37 @@
+x = 9 #I could use any number here. 
+y = 3 
+
+#Arithmetic Operators 
+print(x+y) #Addition 
+print(x-y) #Subtraction 
+print(x*y) #Multiplication 
+print(x/y) #Division 
+print(x%y) #Modulus 
+print(x**y) #Exponentiation 
+x = 9.191823  #wow, that is a very precise number 
+print("Floor divide: ", x//y) #Floor division...how many whole increments of even division 
+print("Modulo: ", x%y)
+
+#Assignment operators 
+x = 9 		# set x = 9 
+x += 3 		# x = x + 3
+print(x)
+x = 9 
+x -= 3 		# x = x -3
+print(x)
+x *= 3 		# x = x * 3
+print(x)
+x /=3 		# x = x/3 
+print(x)
+x**= 3 		# x = x ** 3 
+print(x)
+
+# Comparison Operators 
+x = 9 
+y = 3
+print("Does x==y?", x==y) 	#True if x equals y, False otherwise 
+print("Does x!=y?", x!=y)	#True if x does not equal y, False otherwise 
+print("Is x>y?", x>y) 		#True if x is greater than y, False otherwise 
+print("Is x<y?", x<y) 		#True is x is less than y, False otherwise 
+print("Is x>=y?", x>=y)		#True if x is greater than or equal to y, False otherwise 
+print("Is x<=y?", x<=y)		#True if x is less than or equal to y, False otherwise 
